5aa90154aae56fe76fcf14584263188bdb4e6eb3
davikunzler <126249992+davikunzler@users.noreply.github.com>
2025-03-17 10:06:54 -0300

    Add files via upload

---
+++ db.sql
@@ -0,0 +1,9 @@
+create database menuRestritoDB;
+use menuRestritoDB;
+
+create table cliente(
+    id auto-increment NN,
+    email varcahr primary-key NN,
+    nome varchar NN,
+    senha NN varchar
+);
