d32f0c1630bbbb93d0cff9de8b0850703a7bb271
davikunzler <126249992+davikunzler@users.noreply.github.com>
2025-03-19 19:11:28 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -1,12 +1,26 @@
 const express = require('express');
 const cors = require('cors');
-const mysql = require('mysql2');
+const connection = require('./db_config')
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
+
+
 app.post('/cadastrarCliente', (req, res) => {
     const {email, nome, senha} = req.body;
     
+    if(!email || !nome || !senha){
+        return res.status(400).send({message: 'Todos campos são obrigatórios!'});
+    }
+
+    connection.query('INSERT INTO clientes (email, nome, senha) VALUES (?,?,?)'),
+    [email, nome, senha],
+    (err, resut) => {
+        if (err){
+            console.error('Eroo ao cadastrar novo perfil no DB', err);
+        }
+        res.status(201).send({message: 'Carro cadastrado com sucesso!'})
+    }    
 })
